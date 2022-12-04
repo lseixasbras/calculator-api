edd5c3c784e6a0f326740c811e46b0874a8e3d60
Luis Bras <lseixasbras@gmail.com>
2022-12-04 13:17:32 +0000

    small changes to port

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use((error, req, res, next) => {
     });
   });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Server started at port ${PORT}`));
